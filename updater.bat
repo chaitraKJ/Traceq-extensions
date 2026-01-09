cd4e9c6e98bf3aa2e0845b975d3da5afe625447c
Chaitra K J <chaitra@invictolabs.com>
2026-01-09 13:19:15 +0530

    bat file

--- updater.bat
+++ updater.bat
@@ -1,4 +1,14 @@
-ECHO GeeksforGeeks
 
-prompt.
+REM CHANGE THE DIRECTORY TO THE CHROME FOLDER
+cd C:\Traceq-extensions-main\chrome
+
+REM DOWNLOAD THE ZIP FILE INSIDE THE CHROME FOLDER
+curl "https://extension-provider.onrender.com/traceq-chrome-extended.zip" -O "traceq-chrome-extended.zip" 
+
+REM UNZIP THE FOLDER AND REPLACE THE EXISTING FOLDER CONTENT
+tar -xf traceq-chrome-extended.zip -C "C:\Traceq-extensions-main\chrome\traceq-chrome-extended"
+
+REM DELETE THE DOWNLOADED ZIP FILE
+del traceq-chrome-extended.zip
+
 PAUSE
